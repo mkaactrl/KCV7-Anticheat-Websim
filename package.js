41a5084756c90b57ca4b32689cea2dc067ff9e9a
emkot <flyerccount41@gmail.com>
2025-05-17 02:01:06 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "dependencies": {
+    "express": "^4.18.2",
+    "helmet": "^7.1.0",
+    "dompurify": "^3.0.5",
+    "jsdom": "^22.1.0",
+    "express-rate-limit": "^6.7.0",
+    "csurf": "^1.11.0",
+    "sqlite3": "^5.1.6"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  }
+}
