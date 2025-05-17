b039ee02ae8a64f7fa96ceee7d31182e2dc494a3
emkot <flyerccount41@gmail.com>
2025-05-17 02:05:01 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,14 +1,24 @@
 {
+  "name": "websim-security",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node server/app.js",
+    "dev": "nodemon server/app.js"  # Auto-restarts server on changes
+  },
   "dependencies": {
     "express": "^4.18.2",
     "helmet": "^7.1.0",
     "dompurify": "^3.0.5",
     "jsdom": "^22.1.0",
     "express-rate-limit": "^6.7.0",
-    "csurf": "^1.11.0",
-    "sqlite3": "^5.1.6"
+    "csrf": "^1.11.0",
+    "sqlite3": "^5.1.6",
+    "vm2": "^3.9.19",
+    "acorn": "^8.11.2",
+    "escope": "^3.6.0"
   },
   "devDependencies": {
-    "nodemon": "^3.0.2"
+    "nodemon": "^3.0.2",
+    "morgan": "^1.10.0"
   }
 }
